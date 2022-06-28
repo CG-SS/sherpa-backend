6eca93be4103b2458dcdd1565560462554cb4923
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-06-28 14:22:49 -0300

    Adding logging library

---
+++ libs/logger/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'logger',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/logger',
+};
