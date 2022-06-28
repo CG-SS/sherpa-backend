579363f7cf08d4b5e723577d5edbdefab31eba79
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-06-27 23:30:31 -0300

    Remove unused type

--- apps/services/events/src/app/routes/types.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export type RouteResolver = readonly [
-  string,
-  (req: Request, res: Response) => Promise<void>
-];
--- apps/services/events/src/app/routes/v1/index.ts
+++ apps/services/events/src/app/routes/v1/index.ts
@@ -1,4 +1,4 @@
-import { RouteResolver } from '../types';
+import { RouteResolver } from '@sherpa-backend/services/rest';
 
 import { getAllEvents } from './getAllEvents';
 import { getEvent } from './getEvent';
