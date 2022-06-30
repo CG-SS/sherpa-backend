8f03d78ba7e0488b4fc04a5dc85b780e74856ba7
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-06-30 00:38:20 -0300

    Adding default logging to the default rest app

--- libs/services/rest/src/lib/core/app.ts
+++ libs/services/rest/src/lib/core/app.ts
@@ -1,10 +1,27 @@
-import * as express from 'express';
+import express from 'express';
+import winston from 'winston';
+import expressWinston from 'express-winston';
 import { DefaultGetRoutes } from './routes';
 
 export const defaultApp = express();
 
 // Every microservice will use JSON for their bodies.
 defaultApp.use(express.json());
+defaultApp.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      process.env.NODE_ENV === 'production'
+        ? winston.format.json()
+        : winston.format.simple()
+    ),
+    meta: false,
+    msg: 'HTTP  ',
+    expressFormat: true,
+    colorize: false,
+  })
+);
 
 // Every microservice will have the health route by default.
 // In the future, we might have more endpoints that every microservice that
