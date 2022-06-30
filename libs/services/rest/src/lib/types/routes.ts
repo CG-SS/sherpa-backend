44cf894779ae32467b8f1fc36ce70f7320318a03
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-06-30 00:38:49 -0300

    Adding new type for routes with body schema

--- libs/services/rest/src/lib/types/routes.ts
+++ libs/services/rest/src/lib/types/routes.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
+import { AllowedSchema } from 'express-json-validator-middleware';
 
 export type RouteResolver = readonly [
   string,
+  (req: Request, res: Response) => Promise<void>
+];
+
+export type RouteResolverWithSchema = readonly [
+  string,
+  AllowedSchema,
   (req: Request, res: Response) => Promise<void>
 ];
