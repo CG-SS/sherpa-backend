64f409cff31708a949886206bdb63327b3ce214f
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-07-07 02:52:57 -0300

    Creation of the domain library

---
+++ libs/domain/events/README.md
@@ -0,0 +1,11 @@
+# domain-events
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test domain-events` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint domain-events` to execute the lint via [ESLint](https://eslint.org/).
---
+++ libs/domain/events/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'domain-events',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/domain/events',
+};
