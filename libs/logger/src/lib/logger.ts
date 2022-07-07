47958c3cd3fc460ad3d5b4b6e9150b263d1bbf7b
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-07-07 02:55:17 -0300

    Implementation of the Logger lib

--- libs/logger/src/lib/logger.ts
+++ libs/logger/src/lib/logger.ts
@@ -8,13 +8,8 @@ export const logger = winston.createLogger({
     // Write all logs with importance level of `info` or less to `combined.log`
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-    })
-  );
-}
+    }),
+  ],
+});
