6edb1535ece22d17baa73f8fd4d92e32060b9c8f
Cristiano Guilherme de Souza Silva <criguiss@uol.com.br>
2022-07-07 02:55:52 -0300

    Changes on the rest core

--- libs/services/rest/src/lib/core/app.ts
+++ libs/services/rest/src/lib/core/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import winston from 'winston';
 import expressWinston from 'express-winston';
+import { ValidationError } from 'express-json-validator-middleware';
+
 import { DefaultGetRoutes } from './routes';
 
 export const defaultApp = express();
@@ -10,18 +12,24 @@ defaultApp.use(express.json());
 defaultApp.use(
   expressWinston.logger({
     transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      process.env.NODE_ENV === 'production'
-        ? winston.format.json()
-        : winston.format.simple()
-    ),
+    format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
     meta: false,
     msg: 'HTTP  ',
     expressFormat: true,
     colorize: false,
   })
 );
+defaultApp.use((error, request, response, next) => {
+  // Check the error is a validation error
+  if (error instanceof ValidationError) {
+    // Handle the error
+    response.status(422).send(error.validationErrors);
+    next();
+  } else {
+    // Pass error on if not a validation error
+    next(error);
+  }
+});
 
 // Every microservice will have the health route by default.
 // In the future, we might have more endpoints that every microservice that
